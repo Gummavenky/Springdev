5cad1a4f41102b43e4eda223e7d79c9bb8791886
gummavenky <venkateswarlu.gumma96@gmail.com>
2021-05-22 16:52:44 +0530

    changgexs

--- src/main/java/com/techi/Application.java
+++ src/main/java/com/techi/Application.java
@@ -9,7 +9,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("welcome spring boot");
+<<<<<<< Updated upstream
 		System.out.println("newly modifed");
+=======
+		
+		
+		System.out.println("git staah");
+>>>>>>> Stashed changes
                     
 	
 		
--- src/main/java/com/techi/model/Car.java
+++ src/main/java/com/techi/model/Car.java
@@ -4,6 +4,8 @@ public class Car {
 	public Car() {
 		System.out.println("car class construction");
 		System.out.println("heeloworld");
+		
+		System.out.println("first completion work");
 	}
 
 }
